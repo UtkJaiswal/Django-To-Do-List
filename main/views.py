94d9a333dc5e242e621bce462a2a2a215ad6667f
utkarsh <utkjaiswal58@gmail.com>
2023-07-02 00:37:15 +0530

    Adding the list from browser feature added

--- main/views.py
+++ main/views.py
@@ -7,6 +7,25 @@ from .forms import CreateNewList
 def index(response,id):
     ls =  ToDoList.objects.get(id=id)
     # item=ls.item_set.get(id=1)
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c"+str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt)>2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid input")
+
     return render(response,"main/list.html",{"ls":ls})
 
 
