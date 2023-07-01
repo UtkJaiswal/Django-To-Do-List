4bcff5ab0af3b7c4d9dd8d0f298a21fca50f2555
utkarsh <utkjaiswal58@gmail.com>
2023-07-01 17:39:48 +0530

    templated added and tasks showing completed or not

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ from .models import ToDoList,Item
 def index(response,id):
     ls =  ToDoList.objects.get(id=id)
     # item=ls.item_set.get(id=1)
-    return render(response,"main/base.html",{})
+    return render(response,"main/list.html",{"ls":ls})
 
 
 def home(response):
