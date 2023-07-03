364c3d8015417dd992bcff24b267547cd93ad323
utkarsh <utkjaiswal58@gmail.com>
2023-07-03 12:03:10 +0530

    register form added

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# SuperUser Credentials
+username - admin
+password - admin
+
+username-Tim
+password- Geophysics-1
--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+    'register.apps.RegisterConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK="bootstrap5"
